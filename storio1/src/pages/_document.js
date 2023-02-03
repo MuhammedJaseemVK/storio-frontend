730da369e2783cfea91c08163b633bd022ada097
Jaseem vk <muhammedjaseemvk96@gmail.com>
2023-02-03 11:14:07 +0530

    Initial commit

---
+++ storio1/src/pages/_document.js
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head >
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
+        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+        <script noModule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
---
+++ storio1/src/pages/login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function login() {
+    return (
+
+        <div className='bg-blue-500 text-2xl'>
+            hello
+        </div>
+        <p className='text-grey-500'>
+        Enter your phone
+    </p>
+    )
+}
