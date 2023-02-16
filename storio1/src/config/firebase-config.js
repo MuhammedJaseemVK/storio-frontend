408e45ab4ec6cf52c4d1a53e2f65fe42ffad0b06
Abhinav <abhinav.chek@gmail.com>
2023-02-16 15:26:24 +0530

    Authentication using email and password done

--- storio1/src/config/firebase-config.js
+++ storio1/src/config/firebase-config.js
@@ -1,14 +1,16 @@
 import firebase from 'firebase/app';
-import 'firebase/auth';
+// import { initializeApp, getApps } from "firebase/app"
+// import 'firebase/auth';
+// import 'firebase/app';
+// import 'firebase/auth';
+// import 'firebase/firestore';
+// import 'firebase/storage';
 
+import { initializeApp, getApps } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyBFmKp3aYYXE4kfwjo0zOqvMQvxh39lsdo",
   authDomain: "storio-aaa61.firebaseapp.com",
   projectId: "storio-aaa61",
@@ -16,11 +18,22 @@ const firebaseConfig = {
   messagingSenderId: "202991886717",
   appId: "1:202991886717:web:a2f86b27d583a0ab282c14"
 };
+// if (!firebase.apps.length) {
+//   firebase.initializeApp(firebaseConfig);
+// }
+if (!getApps().length) {
+  initializeApp(firebaseConfig);
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-export default function initFirebase() {
-  if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-  }
 }
+const app = initializeApp(firebaseConfig)
+// const app = firebase.app();
+// const auth = firebase.auth();
+// const db = firebase.firestore();
+// const now = firebase.firestore.Timestamp.now();
+// const storage = firebase.storage();
+const db = getFirestore(app)
+const auth = getAuth(app)
+
+export { db, auth, createUserWithEmailAndPassword }
+// export { auth, db, now, storage };
+console.log(app.name ? 'Firebase Mode Activated!' : 'Firebase not working :(');
--- storio1/src/pages/signup.js
+++ storio1/src/pages/signup.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
-import {MdOutlineArrowForward}  from "react-icons/md";
-import {MdArrowBack}  from "react-icons/md";
+import { MdOutlineArrowForward } from "react-icons/md";
+import { MdArrowBack } from "react-icons/md";
 import Button from '@/components/inputs/Button';
 import Input from '@/components/inputs/Input';
 import { useRouter } from 'next/router';
 import Link from 'next/link'
+import { auth, createUserWithEmailAndPassword } from '../config/firebase-config';
 
 export default function signup() {
   const router = useRouter();
@@ -13,14 +14,25 @@ export default function signup() {
   const [password1, setpassword1] = useState("")
   const [password2, setpassword2] = useState("")
 
+  const signUp = ({ name, email, password }) => {
+    return createUserWithEmailAndPassword(auth, email, password1)
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((error) => {
+        return { error };
+      });
+  };
 
-  function submitHandler(e){
+  function submitHandler(e) {
     e.preventDefault();
-    if(password1!=password2){
+    if (password1 != password2) {
       alert("check passwords")
     }
-    else{
-      router.push('/detailEntry')
+    else {
+      return signUp({name, email, password1}).then((user) => {
+        console.log(user);
+      });
     }
 
   }
@@ -28,21 +40,21 @@ export default function signup() {
   return (
     <div className='bg-black h-screen p-5'>
       <Link href="/" ><MdArrowBack className='text-white text-3xl' /></Link>
-        <div className="flex flex-col gap-3">
-        <div className='text-white text-3xl'>Sign <span className='text-[#ff9900]'>Up</span></div>
+      <div className="flex flex-col gap-3">
+        <div className='text-white text-3xl '>Sign <span className='text-[#ff9900]'>Up</span></div>
         <form onSubmit={submitHandler} className='flex flex-col gap-3 w-full items-center px-3'>
           <Input placeholder='Username' type='text' required={true} value={name} onChange={e => setname(e.target.value)} />
           <Input placeholder='Email' type='text' required={true} value={email} onChange={e => setemail(e.target.value)} />
           <Input placeholder='Password' type='text' required={true} value={password1} onChange={e => setpassword1(e.target.value)} />
           <Input placeholder='Confirm Password' required={true} type='text' value={password2} onChange={e => setpassword2(e.target.value)} />
           <Link href="/" ><p className='text-white text-sm text-right w-full'>Already have an account?</p></Link>
-          <Button text="Continue" />        
+          <Button text="Continue" />
         </form>
-        
-        </div>
-        
-        
-        
+
+      </div>
+
+
+
     </div>
   )
 }
