ca8217a30956d9831d95ee8c554c9a509503645a
Jaseem vk <muhammedjaseemvk96@gmail.com>
2023-02-16 15:47:08 +0530

    aligned text in login

--- storio1/src/pages/signup.js
+++ storio1/src/pages/signup.js
@@ -5,6 +5,8 @@ import Button from '@/components/inputs/Button';
 import Input from '@/components/inputs/Input';
 import { useRouter } from 'next/router';
 import Link from 'next/link'
+import Heading from '@/components/inputs/Heading';
+import Subtext from '@/components/inputs/Subtext';
 
 export default function signup() {
   const router = useRouter();
@@ -29,13 +31,19 @@ export default function signup() {
     <div className='bg-black h-screen p-5'>
       <Link href="/" ><MdArrowBack className='text-white text-3xl' /></Link>
         <div className="flex flex-col gap-3">
-        <div className='text-white text-3xl'>Sign <span className='text-[#ff9900]'>Up</span></div>
+        <Heading text1="Sign" text2="Up" />
+        <Subtext text="Create an account to enjoy shopping" />
+        {/* <div className='text-white text-3xl'>Sign <span className='text-[#ff9900]'>Up</span></div> */}
         <form onSubmit={submitHandler} className='flex flex-col gap-3 w-full items-center px-3'>
           <Input placeholder='Username' type='text' required={true} value={name} onChange={e => setname(e.target.value)} />
           <Input placeholder='Email' type='text' required={true} value={email} onChange={e => setemail(e.target.value)} />
           <Input placeholder='Password' type='text' required={true} value={password1} onChange={e => setpassword1(e.target.value)} />
           <Input placeholder='Confirm Password' required={true} type='text' value={password2} onChange={e => setpassword2(e.target.value)} />
-          <Link href="/" ><p className='text-white text-sm text-right w-full'>Already have an account?</p></Link>
+          
+          <div className='w-full'>
+            <Link href="/"  ><p className='text-white text-sm text-right'>Already have an account?</p></Link>
+          </div>
+        
           <Button text="Continue" />        
         </form>
         
