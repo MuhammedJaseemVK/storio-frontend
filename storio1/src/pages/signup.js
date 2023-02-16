ad9007dc66eddabe3968b8a3a4cb32db04657bca
Jaseem vk <muhammedjaseemvk96@gmail.com>
2023-02-16 14:09:29 +0530

    added links to signup

--- storio1/src/pages/signup.js
+++ storio1/src/pages/signup.js
@@ -1,21 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {MdOutlineArrowForward}  from "react-icons/md";
 import {MdArrowBack}  from "react-icons/md";
 import Button from '@/components/inputs/Button';
+import Input from '@/components/inputs/Input';
+import { useRouter } from 'next/router';
+import Link from 'next/link'
 
 export default function signup() {
+  const router = useRouter();
+  const [name, setname] = useState("")
+  const [email, setemail] = useState("")
+  const [password1, setpassword1] = useState("")
+  const [password2, setpassword2] = useState("")
+
+
+  function submitHandler(e){
+    e.preventDefault();
+    if(password1!=password2){
+      alert("check passwords")
+    }
+    else{
+      router.push('/detailEntry')
+    }
+
+  }
+
   return (
     <div className='bg-black h-screen p-5'>
-        <MdArrowBack className='text-white text-3xl'/>
+      <Link href="/" ><MdArrowBack className='text-white text-3xl' /></Link>
+        <div className="flex flex-col gap-3">
         <div className='text-white text-3xl'>Sign <span className='text-[#ff9900]'>Up</span></div>
-        <div className='flex flex-col gap-3 px-3 py-5'>
-          <input type='text' className='rounded-lg px-3 py-2' placeholder='User name'></input>
-          <input type='text' className='rounded-lg px-3 py-2' placeholder='Phone'></input>
-          <input type='password' className='rounded-lg px-3 py-2' placeholder='Password'></input>
-          <p className='text-white text-sm text-right'>Already have an account?</p>
-          
-          <Button />
+        <form onSubmit={submitHandler} className='flex flex-col gap-3 w-full items-center px-3'>
+          <Input placeholder='Username' type='text' required={true} value={name} onChange={e => setname(e.target.value)} />
+          <Input placeholder='Email' type='text' required={true} value={email} onChange={e => setemail(e.target.value)} />
+          <Input placeholder='Password' type='text' required={true} value={password1} onChange={e => setpassword1(e.target.value)} />
+          <Input placeholder='Confirm Password' required={true} type='text' value={password2} onChange={e => setpassword2(e.target.value)} />
+          <Link href="/" ><p className='text-white text-sm text-right w-full'>Already have an account?</p></Link>
+          <Button text="Continue" />        
+        </form>
+        
         </div>
+        
+        
         
     </div>
   )
