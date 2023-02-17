1647a5f8ddc7247d1dbfc050f513d9fa8c5ace13
Jaseem vk <muhammedjaseemvk96@gmail.com>
2023-02-17 10:58:28 +0530

    hamburger added to customerhome

--- storio1/src/pages/customerhome.js
+++ storio1/src/pages/customerhome.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { RxHamburgerMenu } from "react-icons/rx";
 
 export default function 
 () {
   return (
-    <div className=' '>
-
+    <div className='h-screen bg-black p-3 '>
+      <RxHamburgerMenu className='text-[#ff9900]'/>
+      
     </div>
   )
 }
