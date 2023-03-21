45dd5b67b574828b06b8dd430e586672b72a48c0
Abhinav <abhinav.chek@gmail.com>
2023-03-21 20:14:26 +0530

    Virtual cart skeleton working setup
    :

--- storio1/src/pages/virtual-cart.js
+++ storio1/src/pages/virtual-cart.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-const socket = io("http://localhost:3000/", {
+const socket = io("https://storio.virtualdom.tech", {
     reconnectionDelay: 1000,
     reconnection: true,
     reconnectionAttemps: 10,
@@ -28,7 +28,8 @@ function App() {
             setLastPong(new Date().toISOString());
         });
         socket.on('change', (data) => {
-            setLastPong(data);
+            console.log(data)
+            // setLastPong(data);
         });
 
         return () => {
@@ -39,7 +40,11 @@ function App() {
     }, []);
 
     const sendPing = () => {
-        socket.emit('ping');
+        try {
+            socket.emit('valuefromsocket', { ids: ['sdfs*1000'] });
+        } catch (error) {
+            console.error("Error sending message to server:", error);
+        }
     }
 
     return (
