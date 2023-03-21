7977560b25769ac28805ccc255cf310b04abea07
Abhinav <abhinav.chek@gmail.com>
2023-03-21 23:39:16 +0530

    Added animated cards for remove and add notifications

--- storio1/src/pages/virtual-cart.js
+++ storio1/src/pages/virtual-cart.js
@@ -17,6 +17,26 @@ function App() {
     const [isConnected, setIsConnected] = useState(socket.connected);
     const [lastPong, setLastPong] = useState(null);
     const [products, setproducts] = useState([])
+    const [lastAddedProduct, setlastAddedProduct] = useState({})
+    const [lastRemoved, setlastRemoved] = useState({})
+
+    // global variable
+    let productsArray = []
+
+    function StarRating(props) {
+        const stars = [];
+
+        // Loop through the number of stars and push a star element into the stars array
+        for (let i = 0; i < props.rating; i++) {
+            stars.push(<span key={i}>★</span>);
+        }
+
+        return (
+            <div className="star-rating">
+                {stars}
+            </div>
+        );
+    }
 
     useEffect(() => {
         socket.on('connect', () => {
@@ -31,8 +51,25 @@ function App() {
             setLastPong(new Date().toISOString());
         });
         socket.on('change', (data) => {
-            console.log(data)
+            const removed = productsArray.filter((prevObj) => !data.some((obj) => JSON.stringify(obj) === JSON.stringify(prevObj)));
+            const added = data.filter((prevObj) => !productsArray.some((obj) => JSON.stringify(obj) === JSON.stringify(prevObj)));
+            const prevProductsArrayLen = productsArray.length
+            console.log(prevProductsArrayLen)
             setproducts(data)
+            productsArray = data
+            if (removed[0] && data.length !== 1 && prevProductsArrayLen !==0) {
+                console.log(removed[0].name + " removed")
+                setlastRemoved(removed[0])
+                setTimeout(() => {
+                    setlastRemoved({})
+                }, 3500)
+            }
+            if (added[0]) {
+                setlastAddedProduct(added[0])
+                setTimeout(() => {
+                    setlastAddedProduct({})
+                }, 3000)
+            }
         });
 
         return () => {
@@ -51,31 +88,112 @@ function App() {
     }
 
     return (
-        <div className='w-screen h-screen bg-black text-white p-5'>
+        <div className='w-screen h-screen bg-black text-white p-5 relative'>
             <p>Connected: {'' + isConnected}</p>
             <Heading text1="My" text2="Cart" />
             <div className='pt-10 gap-3'>
-            {
-                products?.map(p=>{
-                    return (
-                        <div className='rounded-md bg-gray-800 '>
-                            <div className='flex'>
-                                <div  className='w-20 h-20'>
-                                    <img src={p.imageUrl} />
-                                </div>
-                                <div className='p-3'>
-                                    <div className=''>
-                                        {p.name}
+                {
+                    products?.map(p => {
+                        return (
+                            <div className='rounded-md bg-gray-500'>
+                                <div className='flex'>
+                                    <div className='w-20 h-20'>
+                                        <img src={p.image} />
                                     </div>
-                                    <div className='font-bold text-xl'>
-                                        {p.mrp}
+                                    <div className='p-3'>
+                                        <div className=''>
+                                            {p.name}
+                                        </div>
+                                        <div className=''>
+                                            {p.brand}
+                                        </div>
+                                        <div className=''>
+                                            {p.description}
+                                        </div>
+                                        <div className='font-bold text-xl'>
+                                            {p.price}
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+                        )
+                    })
+                }
+            </div>
+            <div className='absolute bottom-0 left-0 z-50 w-full px-5 '>
+                {
+                    lastRemoved?.name &&
+                    <div className='bg-white rounded-t-2xl px-5 pt-5 pb-10 animate-emerge flex flex-col gap-5 items-center z-30 absolute bottom-0 right-5 left-5'>
+                        <div className='flex flex-col items-center'>
+                            <div className=' rounded-full w-12 h-12 bg-red-400 grid items-center justify-center'>
+                                X
+                            </div>
+                            <div className='text-red-600'>
+                                Removed
+                            </div>
                         </div>
-                    )
-                })
-            }
+                        <div className='text-gray-700 text-center'>
+                            <span className='font-bold '>
+                                {lastRemoved.name}
+                            </span>
+                            <span> removed from cart</span>
+                        </div>
+                        <div className='text-gray-700 text-center'>
+                            <span className='text-gray-500'>
+                                {lastRemoved.description}
+                            </span>
+                            <div>
+                                {
+                                    <StarRating rating={lastRemoved.rating} />
+                                }
+                            </div>
+                        </div>
+                        <div className='flex text-gray-800 gap-5 items-center'>
+                            <div>{lastRemoved.brand}</div>
+                            <div>
+                                <span className='font-bold text-2xl'>{lastRemoved.price}</span>
+                                <span className='text-sm text-gray-500'>/-</span>
+                            </div>
+                        </div>
+                    </div>
+                }
+                {
+                    lastAddedProduct?.name &&
+                    <div className='bg-white rounded-t-2xl px-5 pt-5 pb-10 animate-emerge flex flex-col gap-5 items-center z-40 absolute bottom-0 right-5 left-5'>
+                        <div className='flex flex-col items-center'>
+                            <div className=' rounded-full w-12 h-12 bg-green-400 grid items-center justify-center'>
+                                ✓
+                            </div>
+                            <div className='text-green-700'>
+                                Added to cart
+                            </div>
+                        </div>
+                        <div className='text-gray-700 text-center'>
+                            <span className='font-bold '>
+                                {lastAddedProduct.name}
+                            </span>
+                            <span> added to cart</span>
+                        </div>
+
+                        <div className='text-gray-700 text-center'>
+                            <span className='text-gray-500'>
+                                {lastAddedProduct.description}
+                            </span>
+                            <div>
+                                {
+                                    <StarRating rating={lastAddedProduct.rating} />
+                                }
+                            </div>
+                        </div>
+                        <div className='flex text-gray-800 gap-5 items-center'>
+                            <div>{lastAddedProduct.brand}</div>
+                            <div className='flex items-center'>
+                                <span className='font-bold text-2xl'>{lastAddedProduct.price}</span>
+                                <span className='text-sm text-gray-500'>/-</span>
+                            </div>
+                        </div>
+                    </div>
+                }
             </div>
         </div>
     );
