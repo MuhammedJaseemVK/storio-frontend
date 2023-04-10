79ecbc63dcf4de8d1821915b5544e384553000f9
Jaseem vk <muhammedjaseemvk96@gmail.com>
2023-04-10 15:01:41 +0530

    clicking outside popup close popup in editprofile

--- storio1/src/components/Popup.js
+++ storio1/src/components/Popup.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 function Popup({visible,onClose,onSubmit}) {
+  const handleOnClose = (e) => {
+    if (e.target.id ==="container") onClose();
+  };
+
     if (!visible) return null;
   return (
-    <div className='fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm text-black flex justify-center items-center'>
+    <div id='container' onClick={handleOnClose} className='fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm text-black flex justify-center items-center'>
         <div className='bg-white p-2 rounded'>
             <p>Confirm edit?</p>
             <div className='flex flex-row gap-5'>
