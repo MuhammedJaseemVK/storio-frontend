625dfe544b5b5ad95972bff982058d7a7587a070
Jaseem vk <muhammedjaseemvk96@gmail.com>
2023-04-18 23:03:47 +0530

    shopping list basic function implemented.needs styling and checkbox

---
+++ storio1/src/pages/mylist.js
@@ -0,0 +1,52 @@
+import React,{useState,useEffect} from 'react'
+
+export default function mylist() {
+    const [task, settask] = useState('');
+    const [tasks, settasks] = useState([]);
+
+    useEffect(()=>{
+        if(localStorage.getItem("localTasks")){
+            const storedList =JSON.parse(localStorage.getItem("localTasks"));
+            settasks(storedList);
+        }
+    })
+    
+    const addTask = (e) => {
+        if(task) {
+            const newtask = {id:new Date().getTime().toString(), title:task };
+            settasks([...tasks,newtask]);
+            localStorage.setItem('localTasks',JSON.stringify([...tasks,newtask]))
+        }
+    }
+    const handleDelete = (task) =>{
+        const deleted = tasks.filter((t) => t.id !== task.id);
+        settasks(deleted);
+        localStorage.setItem('localTasks',JSON.stringify(deleted));
+    }
+  return (
+    <div className='h-screen bg-black text-white'>
+        <h3 className='text-white'>My List</h3>
+        <input type='text' className='text-black' placeholder='Add task' value={task} onChange={(e) => settask(e.target.value)} />
+        <button type='submit' className='bg-[#ff9900]' onClick={addTask}>Add</button>
+
+        {/* Tasks section */}
+        <div>
+            You have 
+            {
+                !tasks.length? " no tasks"
+                :tasks.length === 1?" 1 task"
+                :tasks.length>1 ?  ` ${tasks.length} tasks`
+                : null
+            }
+            <div>
+                {tasks.map((task) =>
+                    <React.Fragment key={task.id}>
+                        {task.title}
+                        <button onClick={() => handleDelete(task)}>Del</button>
+                    </React.Fragment>
+                )}
+            </div>
+        </div>
+    </div>
+  )
+}
