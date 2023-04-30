01ac4dc2dc70f54e118e7dfdf3792f81a8f33369
Jaseem vk <muhammedjaseemvk96@gmail.com>
2023-04-30 12:46:35 +0530

    added styling to qr code.needs to add localStorage

--- storio1/src/components/QRCodeScanner.js
+++ storio1/src/components/QRCodeScanner.js
@@ -4,36 +4,58 @@ import QrScanner from 'qr-scanner';
 const QRCodeScanner = () => {
   const videoRef = useRef(null);
   const [qrList, setQrList] = useState([]);
+  const [shouldScan, setShouldScan] = useState(true);
 
   useEffect(() => {
     QrScanner.WORKER_PATH = './qr-scanner-worker.min.js';
-    const scanner = new QrScanner(videoRef.current, async (result) => {
-      const currentTime = new Date();
-      const newQrList = qrList.slice();
-      const index = newQrList.findIndex((qr) => qr.code === result);
-      if (index !== -1) {
-        newQrList[index].outTime = currentTime;
-      } else {
-        newQrList.push({
-          code: result,
-          inTime: currentTime,
-          outTime: null,
-        });
-      }
-      setQrList(newQrList);
-      // Check if scanner is not null before calling resume
-      
-    });
-    scanner.start();
+    let scanner = null;
+
+    if (shouldScan) {
+      scanner = new QrScanner(videoRef.current, async (result) => {
+        const currentTime = new Date();
+        const newQrList = qrList.slice();
+        const index = newQrList.findIndex((qr) => qr.code === result);
+        if (index !== -1) {
+          newQrList[index].outTime = currentTime;
+        } else {
+          newQrList.push({
+            code: result,
+            inTime: currentTime,
+            outTime: null,
+          });
+        }
+        setQrList(newQrList);
+        setShouldScan(false);
+        setTimeout(() => {
+          setShouldScan(true);
+        }, 5000);
+      });
+      scanner.start();
+    }
+
     return () => {
-      scanner.destroy();
+      if (scanner) {
+        scanner.destroy();
+      }
     };
-  }, [qrList]);
+  }, [qrList, shouldScan]);
 
   return (
     <div>
-      <div>{qrList.map((qr) => JSON.stringify(qr))}</div>
-      <video ref={videoRef} width="50%" height="50%" />
+      <div>
+        {qrList.map((qr) => {
+          return (
+            <div className='p-3 bg-white text-black rounded-md mx-auto w-80 mt-2' key={qr.code}>
+              <p className='font-bold'>User: {qr.code}</p>
+              <div className='flex flex-col gap-2'>
+              <p className='text-[#ff9900]'>Check in: {qr.inTime.toLocaleString()}</p>
+              {qr.outTime ? <p className='text-red-500'> Check out: {qr.outTime.toLocaleString()} </p> : null}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <video ref={videoRef} width='25%' height='25%' autoPlay={true} />
     </div>
   );
 };
