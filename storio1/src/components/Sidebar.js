20af4e846631328bf396bb94f3bffd8005c96189
Jaseem <muhammedjaseemvk96@gmail.com>
2023-05-18 06:45:43 +0530

    started supllier dashbboard

--- storio1/src/components/Sidebar.js
+++ storio1/src/components/Sidebar.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AiFillHome, AiOutlineDropbox, AiFillDollarCircle } from 'react-icons/ai';
 import { IoMdAddCircle } from 'react-icons/io';
 import { MdOutlineQrCodeScanner } from 'react-icons/md';
 import { TbLogout } from 'react-icons/tb';
 import { useRouter } from 'next/router';
+import Popup from '@/components/Popup';
 
 function Sidebar({ activeTab }) {
 
     const router = useRouter();
+    const [popupHeading, setPopupHeading] = useState('Are you sure you want to logout?');
+    const [showPopup, setshowPopup] = useState(false);
+    const handleOnClose = () => setshowPopup(false);
 
     const Logout = (e) => {
         e.preventDefault();
@@ -18,12 +22,15 @@ function Sidebar({ activeTab }) {
 
     return (
         <div className=' left-0 bg-gray-900 h-screen w-18 flex flex-col items-center justify-between py-8 gap-8'>
+
             <div className='flex flex-col gap-8 w-full'>
                 <div className="w-full flex justify-center gap-y-5  gap-1">
                     <AiFillHome className="text-[#ff9900] text-2xl" />
                     <span className="text-2xl font-bold text-[#ff9900]">STORIO</span>
                 </div>
                 <div className="flex justify-center mt-8">
+
+                        <Popup heading={popupHeading} visible={showPopup} onClose={handleOnClose} onSubmit={Logout} />
                     <ul className="list-none flex flex-col gap-4 px-3">
                         <li className={`py-2 px-8 rounded-md ${activeTab === 1 ? 'text-[#ff9900] ' : 'text-white '} hover:text-[#ff9900] hover:bg-white "`}>
                             <a href="/shopownerhome" className="flex flex-col items-center justify-center gap-2 ">
@@ -58,12 +65,17 @@ function Sidebar({ activeTab }) {
                     </ul>
                 </div>
             </div>
-            <button className="py-2.5 px-12.5 ml-[-2rem]" onClick={Logout}>
+            
+            <button className="py-2.5 px-12.5 ml-[-2rem]" onClick={(e) => {
+                e.preventDefault()
+                setshowPopup(true)
+            }}>
                 <div className="text-white flex items-center justify-center gap-2 ">
                     <TbLogout className=" text-2xl" />
                     <span className='text-xl font-bold'>Logout</span>
                 </div>
             </button>
+
         </div>
     )
 }
--- storio1/src/pages/supplierHome.js
+++ storio1/src/pages/supplierHome.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import Sidebar from '@/components/Sidebar'
+import Navbar from '@/components/Navbar'
+
 
 export default function inventory() {
     const [products, setproducts] = useState([])
@@ -24,36 +27,48 @@ export default function inventory() {
 
     return (
 
-        <div className=" bg-black mx overflow-y-auto h-screen">
+        <div className='h-screen w-full bg-black'>
+            <div className='flex flex-row'>
+                <Sidebar activeTab={3} />
+                <div className='w-full flex flex-col'>
+                    <Navbar section="Add product" />
+                    <div className='my-auto'>
 
-            <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-                <h2 className='text-2xl text-white font-bold  p-5 '> Stock Details</h2>
-                <table className="w-full text-white bg-black">
-                    <thead>
-                        <tr className="bg-[#ff9900]">
-                            <th className="py-2 px-4  font-bold">Product Name</th>
-                            <th className="py-2 px-4  font-bold">Description</th>
-                            <th className="py-2 px-4  font-bold">Price</th>
-                            <th className="py-2 px-4  font-bold">Brand</th>
-                            <th className="py-2 px-4  font-bold">Category</th>
-                            <th className="py-2 px-4  font-bold">Quantity</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {products.map(p =>
-                            <tr key={p.name} className="bg-gray-700 hover:bg-gray-800">
-                                <td className="py-2 px-4">{p.name}</td>
-                                <td className="py-2 px-4">{p.description}</td>
-                                <td className="py-2 px-4">{p.price}</td>
-                                <td className="py-2 px-4">{p.brand}</td>
-                                <td className="py-2 px-4">{p.category}</td>
-                                <td className={`py-2 px-4 ${p.quantity <= 5 ? 'text-red-500' : ''}`}>{p.quantity}</td>
-                            </tr>
-                        )}
-                    </tbody>
-                </table>
+                        <div className=" bg-black mx overflow-y-auto h-screen">
+
+                            <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+                                <h2 className='text-2xl text-white font-bold  p-5 '> Stock Details</h2>
+                                <table className="w-full text-white bg-black">
+                                    <thead>
+                                        <tr className="bg-[#ff9900]">
+                                            <th className="py-2 px-4  font-bold">Product Name</th>
+                                            <th className="py-2 px-4  font-bold">Description</th>
+                                            <th className="py-2 px-4  font-bold">Price</th>
+                                            <th className="py-2 px-4  font-bold">Brand</th>
+                                            <th className="py-2 px-4  font-bold">Category</th>
+                                            <th className="py-2 px-4  font-bold">Quantity</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {products.map(p =>
+                                            <tr key={p.name} className="bg-gray-700 hover:bg-gray-800">
+                                                <td className="py-2 px-4">{p.name}</td>
+                                                <td className="py-2 px-4">{p.description}</td>
+                                                <td className="py-2 px-4">{p.price}</td>
+                                                <td className="py-2 px-4">{p.brand}</td>
+                                                <td className="py-2 px-4">{p.category}</td>
+                                                <td className={`py-2 px-4 ${p.quantity <= 5 ? 'text-red-500' : ''}`}>{p.quantity}</td>
+                                            </tr>
+                                        )}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-    )
+            </div>
+
+            )
 }
 
